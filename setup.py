2a62c3387c682a001cba9d766606e9247a45631b
Rafael Alvarado <ontoligent@gmail.com>
2022-05-06 09:27:47 -0400

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(name='Mazo',
+      version='1.0',
+      description='Mazo makes MALLET easy.',
+      author='Raf Alvarado',
+      author_email='ontoligent@gmail.com',
+      url='https://github.com/ontoligent/mazo',
+      py_modules=['polite.polite'],
+      scripts=['mazo']
+     )
